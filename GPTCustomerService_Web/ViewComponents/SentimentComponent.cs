9fb1d4c9d5332fd19c4ed0470f0ce650a048bf1a
Frederic Wickert <frederic.wickert@live.fr>
2023-10-03 12:19:46 +0200

    Sentiment OK

---
+++ GPTCustomerService_Web/ViewComponents/SentimentComponent.cs
@@ -0,0 +1,31 @@
+﻿using GPTCustomerService_Web.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GPTCustomerService_Web.ViewComponents
+{
+    public class SentimentComponent : ViewComponent
+    {
+        private readonly CustomResponse _sentimentResponse;
+
+        public SentimentComponent([FromServices] CustomResponse sentimentResponse)
+        {
+            _sentimentResponse = sentimentResponse;
+            _sentimentResponse.SkillName = "TextAnalysisSkill";
+            _sentimentResponse.FunctionName = "Sentiment";
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string input, string language)
+        {
+
+            await _sentimentResponse.GetAsync("Sentiment", 
+                               new Dictionary<string, string>()
+                               {
+                    { "input",input },
+                    { "language", language } 
+                });
+            return View();
+        }
+
+
+    }
+}
